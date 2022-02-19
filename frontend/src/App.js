e3f0f1b126e90229f8cefb144f74040db7b57afc
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-19 11:39:19 +0530

    adding the updated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import "./bootstrap.min.css"
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="App">Hello World</div>
+      <main style={{ minHeight: "93vh" }}></main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
